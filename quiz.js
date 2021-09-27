908285023ecf94605f3802a235a6cc4845d8512f
Shivangi32 <sdatisha.2195@gmail.com>
2021-09-27 08:01:47 +0530

    commit

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 var questions=[
-    "It is not possible to achieve inheritance of structures in c++?",
+    "It is not possible to achieve inheritance of structures in c++.",
     "Super classes are also called Parent classes/Base classes",
     "Interpreter programs execute high-level language programs without compiling them into machine language first.",
     "To execute an application in a Windows command prompt, you must specify the name of the application followed by the .exe extension.",
@@ -20,6 +20,24 @@ var True=document.getElementById("true");
 var False=document.getElementById("false");
 var question= document.getElementById("question");
 var user_score=document.getElementById("user_score");
+
 console.log(question);
-for(var i=0;i<questions.length;i++)
-   question.textContent="Q"+i+"."+questions[i];
+var counter=0;
+question.textContent="Q"+(counter+1)+"."+questions[0];
+
+function onSubmit(){
+
+}
+
+True.addEventListener("click",True.classList.add("colourchange"));
+submit.addEventListener("click",onSubmit());
+next.addEventListener("click",function(){
+    True.addEventListener("click",True.classList.remove("colourchange"));
+    if(counter<9)
+    {
+     counter++;
+     question.textContent="Q"+(counter+1)+"."+questions[counter];
+    }
+    else
+    onSubmit();
+})
