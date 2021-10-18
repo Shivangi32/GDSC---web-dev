3be1e69ed8288bf2c1b2a2097918fb32541bae59
Shivangi32 <sdatisha.2195@gmail.com>
2021-10-18 14:05:31 +0530

    commt

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -12,7 +12,8 @@ var riddles=["What can you break, even if you never pick it up or touch it?",
 
 var letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 window.onload=reset;
-var lives=5;
+
+var lives=7;
 var idx=Math.floor(Math.random() * words.length);
 var curr_word = words[idx];
 var riddle= riddles[idx];
@@ -21,26 +22,9 @@ var answerArray = [];
 var word=document.getElementById("word");
 var cell=document.getElementsByClassName("cell");
 var new_word=document.getElementById("new_word");
-
-function reset(){
-  lives=5;
-  idx=Math.floor(Math.random() * words.length);
-  curr_word = words[idx];
-  riddle= riddles[idx];
-  question.textContent=riddle;
-  for (var i = 0; i < curr_word.length; i++) {
-    answerArray[i] = "_";
-    }
-   word.textContent=answerArray;
-  for(i=0;i<lives;i++)
-    cell[i].style.visibility="visible";
-
-}
-
-new_word.addEventListener("click",reset);
-
-
+var warning=document.getElementById("warning");
 var alphabets=document.getElementById("alphabets");
+
 for(var i=0;i<26;i++)
 {
     var btn=document.createElement("button");
@@ -50,8 +34,37 @@ for(var i=0;i<26;i++)
     btn.addEventListener("click",(event)=>btnclick(event));
 }
 
-function btnclick(e){
+var btn=document.getElementsByClassName("btn");
 
+function reset(){
+  lives=7;
+  idx=Math.floor(Math.random() * words.length);
+  curr_word = words[idx];
+  riddle= riddles[idx];
+  question.textContent=riddle;
+  answerArray=[];
+  
+  for (var i = 0; i < curr_word.length; i++)
+    answerArray[i] = "_";
+  
+  word.textContent=answerArray;
+
+  for(i=0;i<lives;i++)
+    cell[i].style.visibility="visible";
+
+  for(i=0;i<btn.length;i++)
+    btn[i].classList.remove("selected");
+
+  warning.textContent="You have "+lives+" lives";
+}
+
+new_word.addEventListener("click",reset);
+
+function btnclick(e){
+    if(e.target.classList.contains("selected"))
+    {
+      return;
+    }
     e.target.classList.add("selected");
     var ch=e.target.innerHTML;
     if(curr_word.indexOf(ch)==-1)
@@ -69,5 +82,12 @@ function btnclick(e){
           }
       }
       word.textContent=answerArray;
+    }
+    if(lives==0)
+    {
+       warning.textContent="You lost :(";
+    }
+    else{
+      warning.textContent="You have "+lives+" lives";
     }
 }
